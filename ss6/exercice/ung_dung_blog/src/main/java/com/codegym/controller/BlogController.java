c889d46ce53b57b01cf6d1483263527b3d87076f
quoctrungdn102 <trungtranfpt12345@gmail.com>
2022-06-21 20:49:40 +0700

    first commit

--- ss6/exercice/ung_dung_blog/src/main/java/com/codegym/controller/BlogController.java
+++ ss6/exercice/ung_dung_blog/src/main/java/com/codegym/controller/BlogController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -55,8 +56,8 @@ public class BlogController {
         return "redirect:/";
     }
 
-    @GetMapping("/delete/{id}")
-    public String deleteBlog(@PathVariable("id") Integer id) {
+    @GetMapping("/delete")
+    public String deleteBlog(@RequestParam("idBlog") Integer id) {
         iBlogService.delete(id);
         return "redirect:/";
     }
